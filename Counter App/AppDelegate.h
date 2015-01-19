20823509af932607bc1d5f040ee427a7ad59a60c
Didats Triadi <didats@gmail.com>
2015-01-19 10:40:59 +0700

    First commit

---
+++ Counter App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Counter App
+//
+//  Created by Didats on 19/01/15.
+//  Copyright (c) 2015 Rimbunesia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Counter App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Counter App
+//
+//  Created by Didats on 19/01/15.
+//  Copyright (c) 2015 Rimbunesia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
